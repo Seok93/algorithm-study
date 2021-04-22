c93b513264b412957140782ffbb758b876b6918b
Suk Woo <gkdltjr@naver.com>
2021-04-22 15:28:41 +0900

    Update 03_get_melon_best_album.py

--- sparta_algorithm_lecture/week_03/homework/03_get_melon_best_album.py
+++ sparta_algorithm_lecture/week_03/homework/03_get_melon_best_album.py
@@ -7,35 +7,35 @@ plays = [500, 600, 150, 800, 2500]
 def get_melon_best_album(genre_array, play_array):
     result = []
 
-    # 가장 많이 재생된 장르 찾기
-    genre_dict = {}
-    genre_plays = {}
-    play_idx = 0
-    for genre in genre_array:
-        if genre in genre_dict:
-            genre_dict[genre] += play_array[play_idx]
-            genre_plays[genre].append((play_idx, play_array[play_idx]))
+    genres_total_count_dict = {}  # 장르별 재생 횟수
+    genres_play_list_dict = {}  # 장르별 음악 리스트
+
+    for i in range(len(genres)):
+        genre = genres[i]
+        play = plays[i]
+        if genre not in genres_total_count_dict.keys():
+            genres_total_count_dict[genre] = play
+            genres_play_list_dict[genre] = [(i, play)]
         else:
-            genre_dict[genre] = play_array[play_idx]
-            genre_plays[genre] = [(play_idx, play_array[play_idx])]
-        play_idx += 1
+            genres_total_count_dict[genre] += play
+            genres_play_list_dict[genre].append((i, play))
 
     # 장르별 재생 횟수 정렬
-    for key in genre_plays.keys():
-        data = genre_plays[key]
-        data =sorted(data, key=lambda x: x[1])
-        genre_plays[key] = data
+    sorted_genre = sorted(genres_total_count_dict.items(), key=lambda item: item[1], reverse=True)
 
-    # 장르 내에서 많이 재생된 노래 2개씩 추출
-    res = sorted(genre_dict, key=lambda x:x[0], reverse=True)
-    for key in res:
-        target = genre_plays[key]
-        cnt = 0
-        while cnt != 2:
-            if len(target) != 0:
-                play = target.pop()
-                result.append(play[0])
-            cnt += 1
+    # 장르 내 재생 횟수 정렬
+    for key in genres_play_list_dict.keys():
+        data = genres_play_list_dict[key]
+        data = sorted(data, key=lambda x: (x[1], -x[0]))
+        genres_play_list_dict[key] = data
+
+    # 장르별 최다 재생곡 2개씩 추출
+    for key, value in sorted_genre:
+        target = genres_play_list_dict[key]
+        extract_cnt = 2
+        while len(target) > 0 and extract_cnt > 0:
+            result.append(target.pop()[0])
+            extract_cnt -= 1
 
     return result
 
