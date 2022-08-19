9b92029ff4a7a8a56c080d40e9d89ac20f7ca16d
Seok93 <gkdltjr@naver.com>
2022-08-19 20:53:33 +0900

    해시: 위장 문제 풀이 추가

--- programmers/hash/위장/solution.js
+++ programmers/hash/위장/solution.js
@@ -1,19 +1,22 @@
 function solution(clothes) {
-    let answer = 1;
-    let categories = new Map();
+  var answer = 1;
+  const categories = new Map();
 
-    for(let [name, type] of clothes) {
-        if(categories.has(type)) {
-            categories.set(type, categories.get(type)+1);
-        }else {
-            categories.set(type, 1);
-        }
+  // 1. 옷의 가짓수를 구한다.
+  for (let [name, type] of clothes) {
+    if (categories.has(type)) {
+      categories.set(type, categories.get(type) + 1);
+    } else {
+      categories.set(type, 1);
     }
+  }
 
-    for(let value of categories.values()) {
-        answer *= value + 1;
-    }
-    answer -= 1
+  // 2. 옷의 매칭 가능 조합의 수를 구한다.
+  for (let value of categories.values()) {
+    const caseCount = value + 1; // 아무것도 선택하지 않는 경우의 수 추가
+    answer *= caseCount;
+  }
+  answer -= 1; // 최소 1개는 입어야하니, 아무것도 안입은 경우의 수 1개 빼기
 
-    return answer;
+  return answer;
 }
